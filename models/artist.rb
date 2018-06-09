ebc154024e50e2cd989c057b75082b29a86bc84e
Greg Rutherford <g.rutherford10@gmail.com>
2018-06-09 16:35:31 +0100

    add class files and test specs

---
+++ models/artist.rb
@@ -0,0 +1,9 @@
+require_relative('../db/sql_runner')
+
+class Artist
+
+  def initialize(options)
+
+  end
+
+end
---
+++ specs/testing_specs.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../models/artist')
+require_relative('../models/exhibit')
+
+class TestArtist < MiniTest::Test
+
+
+end
+
+class TestExhibit < MiniTest::Test
+
+
+end  
