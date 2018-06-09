84330d437e3d78535911b586ce487d8d666417db
Greg Rutherford <g.rutherford10@gmail.com>
2018-06-09 16:22:09 +0100

    add gallery.sql that contains tables

---
+++ db/gallery.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS exhibits;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  nationality VARCHAR(255),
+  craft VARCHAR(255)
+);
+
+CREATE TABLE exhibits (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  year_created VARCHAR(255),
+  medium VARCHAR(255),
+  artist_id INT REFERENCES artists(id)
+);
